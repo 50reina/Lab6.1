ce57fa393542eff118bf0291d72cff3a8fb7d5a1
50reina <nrr.nya@gmail.com>
2024-08-20 20:46:44 -0700

    Completed modifications as per lab instructions

---
+++ js/aboutme.js
@@ -0,0 +1,7 @@
+let firstName = "Reina";
+let lastName = "Koh";
+let programName = "CyberSecurity Operations";
+
+document.getElementById("fullName").textContent = firstName + " " + lastName;
+document.getElementById("fullName1").textContent = firstName + " " + lastName;
+document.getElementById("program").textContent = programName;
---
+++ js/script.js
@@ -0,0 +1,2 @@
+let programmingLag = prompt("What is your favorite language ?");
+document.getElementById("favProg").textContext = programmingLag;
